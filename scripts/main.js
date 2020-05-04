9936797fa760d1f274bce681e690dd8bcc614c1f
vxsudoxv <[vxsudoxvru@gmail.com]>
2020-05-04 03:28:53 -0400

    added side bare overlay, a few comments in js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
-let sidnav =document.querySelector("#side_navigation")
+//get side navigation div
+let sidnav =document.querySelector("#side_navigation");
+//get activator button for side nav
 let sideactive = document.querySelector("#activate");
-sideactive.onclick = function(event){
+// when side nav burger is prssed it adds a class if it doesn't exist and removes it if it exist.
+let sidestuff = (event) =>{
     sidnav.classList.toggle("open");
+    side_overlay.classList.toggle("hidcontent")
     event.preventDefault();
+};
+
+sideactive.onclick=() =>{
+    sidestuff();
+};
+// creating overlayer to be added when side nav is opened.
+
+//get main div
+let get_main = document.querySelector("main");
+//create the div for the overlay
+let side_overlay = document.createElement("div");
+//add div to main.
+get_main.appendChild(side_overlay);
+//add class for stylying with css.
+side_overlay.classList.add("side_overlay")
+//if side overlay is clicked side navigation will toggle.
+side_overlay.onclick = () =>{
+    sidestuff();
 }
